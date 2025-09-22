6940231aeccac59782519433f00532b643dcf0f6
Fiona Luo <fiona.luo@gmail.com>
2025-09-22 14:15:37 -0400

    updates

--- utils.py
+++
@@ -1,3 +0,0 @@
-def soft_update(target, source, tau=0.005):
-    for target_param, src_param in zip(target.parameters(), source.parameters()):
-        target_param.data.copy_(target_param.data * (1.0 - tau) + src_param.data * tau)
